84848e433182db6cb78f196fdefb27097b43e87e
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-06-18 16:55:35 -0300

    feat: Base do algoritimo funcionando

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { Box, Button, FormControlLabel, MenuItem, Radio, RadioGroup, Select, Stack, Typography } from "@mui/material"
+import { useState } from "react"
+import { vetor } from "./valores"
+
+
+function App() {
+
+  const algoritimos = [
+    {
+      value: 'Selection Sort',
+      label: 'Selection Sort',
+    },
+    {
+      value: 'Insertion Sort',
+      label: 'Insertion Sort',
+    },
+    {
+      value: 'Bubble Sort',
+      label: 'Bubble Sort',
+    },
+    {
+      value: 'Shell Sort',
+      label: 'Shell Sort',
+    },
+    {
+      value: 'Quick Sort',
+      label: 'Quick Sort',
+    }
+  ]
+
+  const [algEscolhido, setAlgEscolhido] = useState(
+    {
+      campo1: 'Selection Sort',
+      campo2: 'Selection Sort'
+    }
+  )
+    
+  const aoEscolher = (evento) => {
+    const { name, value } = evento.target
+    setAlgEscolhido({
+      ...algEscolhido,
+      [name]: value
+    })
+  }
+
+  return (
+    <Box sx={{background: '#F6F2FF', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <button onClick={vetor}>teste</button>
+      <Box sx={{background: '#DCCCFF', width: '450px', height: '550px', maxHeight: '70%', maxWidth: '70%', borderRadius: '30px', boxShadow: 10}}>
+        <Box sx={{margin: 3.5}}>
+          <form >
+
+          <Typography variant="h5" sx={{textAlign: 'center', marginBlock: 2}}>Algoritimos de ordenação</Typography>
+
+          <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 2}}>
+            <Select sx={{width: '40%'}} name="campo1" onChange={aoEscolher} value={algEscolhido.campo1}>
+              {algoritimos.map((opcao) => (
+                <MenuItem value={opcao.value}>{opcao.label}</MenuItem>
+              ))}
+            </Select>
+            <Select sx={{width: '40%'}} name="campo2" onChange={aoEscolher} value={algEscolhido.campo2}>
+              {algoritimos.map((opcao) => (
+                <MenuItem value={opcao.value}>{opcao.label}</MenuItem>
+              ))}
+            </Select>
+          </Box>
+            <Typography variant="body1" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', marginTop: 2}}>Quantidade de nós</Typography>
+            <RadioGroup row sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <FormControlLabel value="1000" control={<Radio />} label="1000" />
+                <FormControlLabel value="10000" control={<Radio />} label="10000" />
+                <FormControlLabel value="100000" control={<Radio />} label="100000" />
+                <FormControlLabel value="1000000" control={<Radio />} label="1000000" />
+            </RadioGroup>
+    
+            <Typography variant="body1" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', marginTop: 2}}>Disposição dos dados</Typography>
+            <RadioGroup sx={{display: 'flex', alignItems: 'start', justifyContent: 'center', marginLeft: 1}}>
+                <FormControlLabel value="ordenado-crescente" control={<Radio />} label="Ordenados - ordem crescente" />
+                <FormControlLabel value="ordenado-decrescente" control={<Radio />} label="Ordenados - ordem decrescente" />
+                <FormControlLabel value="ordenado-aleatória" control={<Radio />} label="Ordem Aleatória" />
+            </RadioGroup>
+
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+              <Button variant="contained" type="submit" sx={{background: '#8B80F9'}}>Executar</Button>
+            </Box>
+
+              </form>
+        </Box>
+
+      </Box>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/valores.js
@@ -0,0 +1,156 @@
+//Algoritimo SelectionSort
+export const SelectionSort = (array) => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let i, j, temp, menor;
+    let n = array.length;
+
+    for (i = 0; i < n - 1; i++) {
+        menor = i;
+        for (j = i + 1; j < n; j++) {
+            if (array[j] < array[menor]) {
+                menor = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[menor];
+        array[menor] = temp;
+    }
+    return(array)
+}
+
+//Algoritimo InsertionSort
+const InsertionSort = (array) => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let i, chave, j;
+    let n = array.length;
+
+    for (i = 1; i < n; i++) {
+        chave = array[i];
+        j = i - 1;
+
+        while (j >= 0 && array[j] > chave) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+
+        array[j + 1] = chave;
+    }
+    
+    return(array)
+
+}
+
+//Algoritimo BubbleSort
+const BubbleSort = (array) => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let i, j, temp;
+    let n = array.length;
+
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return(array)
+
+}
+
+//Algoritimo ShellSort
+export const ShellSort = (array) => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let n = array.length;
+
+        for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2)) {
+            for (let i = gap; i < n; i++) {
+                let temp = array[i];
+                let j;
+                for (j = i; j >= gap && array[j-gap] > temp; j -= gap)
+                    array[j] = array[j-gap];
+    
+                array[j] = temp;
+            }
+
+            return(array)
+        }}
+
+//Algoritimo QuickSort
+export const QuickSort = (array) => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let pivot = array[0]
+
+    let left = []
+    let right = []
+
+    for (let i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i])
+    }
+    console.log('RETORNO DA FUNÇÃO')
+
+    return QuickSort(left).concat(pivot, QuickSort(right))
+}
+const ConverterVetor = async (lista, args) => {
+    for (let i in lista) {
+        let vetNumRandom = lista[i]
+        let nums = vetNumRandom.split('\n')
+        for (let j in nums) {
+            let num = parseInt(nums[j])
+            args.push(num)
+        }
+    }
+    return [...args]
+};
+
+
+let resNumerosAleatorios = await fetch('numeros_aleatorios.txt')
+.then((data) => data.text())
+.then((x) => x.split('\n'))
+
+let resNumerosDecrescentes = await fetch('numeros_ordem_decrescente.txt')
+.then((data) => data.text())
+.then((x) => x.split('\n'))
+
+let resNumerosOrdenados = await fetch('numeros_ordenados.txt')
+.then((data) => data.text())
+.then((x) => x.split('\n'))
+
+
+let listaAleatoria = []
+let listaDescrescente = []
+let listaOrdenada = []
+
+// Converte o vetor de texto para um vetor de inteiros
+
+
+listaAleatoria = await ConverterVetor(resNumerosAleatorios, listaAleatoria)
+listaDescrescente = ConverterVetor(resNumerosDecrescentes, listaDescrescente)
+listaOrdenada = ConverterVetor(resNumerosOrdenados, listaOrdenada)
+
+export const vetor = () => {
+    console.log("LISTA ALEATÓRIA", SelectionSort(listaAleatoria))
+    console.log("LISTA ALEATÓRIA",listaAleatoria)
+}
+
+
+
+
+
